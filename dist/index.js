a570faa67e6fc098af4555b9307a5ad32be9a8c1
Craig Loewen <crloewen@microsoft.com>
2023-11-10 22:16:46 -0500

    New

--- dist/index.js
+++ dist/index.js
@@ -32593,22 +32593,33 @@ try {
 
     // Send a GET request to the API
     axios.get(url).then(response => {
-        // Set the response data as the output
         // Check if success is false
         if (response.data.success === false) {
+            core.info(JSON.stringify(response.data, null, 2));
+            core.info(url);
             core.setFailed("API request was not successful");
-            core.info(JSON.stringify(response.data, null, 2));
-            core.setOutput("message", JSON.stringify(response.data, null, 2));
         } else {
-            // Set the response data as the output
-            core.info("Success Job");
-            core.info(JSON.stringify(response.data, null, 2));
-            core.setOutput("message", JSON.stringify(response.data, null, 2));
+            // Filter the similarIssues array
+            const similarIssues = response.data.similarIssues.filter(issue => issue.score !== 0 && issue.score <= 0.75);
+
+            // Check if the filtered array has a length of 0
+            if (similarIssues.length === 0) {
+                core.setFailed("No similar issues found");
+            } else {
+                // Format the output message
+                let message = "Here are the most similar issues:\n";
+                similarIssues.forEach(issue => {
+                    message += `- [${issue.title} (#${issue.number})](${issue.html_url})\n`;
+                });
+
+                // Set the output message
+                core.info("Success Job");
+                core.info(message);
+                core.setOutput("message", message);
+            }
         }
     }).catch(error => {
         core.setFailed(error.message);
-        core.info("Failed Job");
-        core.info(error.message);
     });
 } catch (error) {
     core.setFailed(error.message);
